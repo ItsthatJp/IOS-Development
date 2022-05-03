fb5e114114b7f7972fb7eb4d97bec10d0ba17b5a
ItsthatJp <90420484+ItsthatJp@users.noreply.github.com>
2022-05-03 12:06:31 -0400

    Add files via upload

---
+++ List_Tabares/List_Tabares/ToDo.swift
@@ -0,0 +1,48 @@
+//
+//  ToDo.swift
+//  List_Tabares
+//
+//  Created by Jp Tabares on 11/21/21.
+//
+
+import Foundation
+
+struct ToDo: Codable {
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+    static let DocumentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("todos").appendingPathExtension("plist")
+    
+    static func loadToDos() -> [ToDo]? {
+        guard let codedToDos = try? Data(contentsOf: ArchiveURL) else { return nil }
+        
+        let propertyListDecoder = PropertyListDecoder()
+        return try? propertyListDecoder.decode(Array<ToDo>.self, from: codedToDos)
+    }
+    
+    static func loadSampleToDos() -> [ToDo] {
+        return [
+            ToDo(title: "This is sample", isComplete: false, dueDate: Date(), notes: "sample notes "),
+            ToDo(title: "Two", isComplete: false, dueDate: Date(), notes: "2"),
+            ToDo(title: "this one should be done", isComplete: true, dueDate: Date(), notes: "lets see")
+        ]
+    }
+    
+    static func saveToDos(_ todos: [ToDo]) {
+        let propertyListEncoder = PropertyListEncoder()
+        let codedToDos = try? propertyListEncoder.encode(todos)
+        try? codedToDos?.write(to: ArchiveURL, options: .noFileProtection)
+    }
+    
+    static let dueDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
+}
+
---
+++ List_Tabares/List_Tabares/ToDoCell.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoCell.swift
+//  List_Tabares
+//
+//  Created by Jp Tabares on 11/21/21.
+//
+
+
+import UIKit
+
+protocol ToDoCellDelegate {
+    func checkMarkTapped(_ sender: ToDoCell)
+}
+
+class ToDoCell: UITableViewCell {
+    
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var delegate: ToDoCellDelegate?
+    
+    @IBAction func isCompleteButtonTapped(_ sender: UIButton) {
+        delegate?.checkMarkTapped(self)
+    }
+}
